05f9c80f0391a50225d75177fe942e558e1efa5b
Kay <109297172+Blueming9511@users.noreply.github.com>
2024-03-18 12:49:25 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector(".sidebar"),
+toggle = document.querySelector(".toggle"),
+text = document.querySelector(".text");
+toggle.addEventListener("click", () => {
+
+sidebar.classList.toggle("close")
+})
